bd9436273b900b07c3031db96a2e7a6695870831
Annapetr0 <45635544+AnnaPetr0@users.noreply.github.com>
2025-04-24 23:40:48 +0300

    1

--- SQL final exam/sneakerShop/server/src/index.ts
+++ SQL final exam/sneakerShop/server/src/index.ts
@@ -6,8 +6,6 @@ import sneakersRoutes from './routes/sneakers';
 import ordersRoutes from './routes/orders';
 import orderItemsRoutes from './routes/orderItems';
 import wishlistRoutes from './routes/wishlist';
-import authRoutes from './routes/auth';
-
 
 
 
@@ -26,7 +24,6 @@ app.use('/api/sneakers', sneakersRoutes);
 app.use('/api/orders', ordersRoutes);
 app.use('/api/order-items', orderItemsRoutes);
 app.use('/api/wishlist', wishlistRoutes);
-app.use('/api/auth', authRoutes);
 
 
 
@@ -34,6 +31,10 @@ app.use('/api/auth', authRoutes);
 app.get('/', (_req, res) => {
   res.send('Sneaker API is running!');
 });
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+}); 
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
